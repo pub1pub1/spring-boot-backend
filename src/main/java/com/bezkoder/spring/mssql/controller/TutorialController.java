54c69eaac4243c6e8c9f3bba95e3b7b3f912ac94
Faheem Javed <faheem.javed@webagesolutions.com>
2023-06-16 13:25:37 -0400

    configured CORS

--- src/main/java/com/bezkoder/spring/mssql/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/mssql/controller/TutorialController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bezkoder.spring.mssql.model.Tutorial;
 import com.bezkoder.spring.mssql.repository.TutorialRepository;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
